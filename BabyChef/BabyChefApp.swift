0189c62d7088481e29e2c65f4bbe51229a3234ec
Hugo F <88924188+hfontvieille@users.noreply.github.com>
2023-08-24 20:02:24 +0200

    Initial Commit

---
+++ BabyChef/BabyChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyChefApp.swift
+//  BabyChef
+//
+//  Created by Hugo Fontvieille on 24/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BabyChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
